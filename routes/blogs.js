676acb176af5f95d37000f97ea2d393d0437920c
Hojong Yu <hojongyu@Hojongs-MBP.home>
2022-07-11 10:59:54 -0400

    assignment done without stretch goals

--- routes/blogs.js
+++ routes/blogs.js
@@ -7,7 +7,6 @@ router.get('/hello-blogs', function(req, res, next) {
 })
 
 router.get('/all-blogs', async function(req, res, next) {
-
     try {
 
         const limit = Number(req.query.limit)
@@ -41,40 +40,39 @@ router.get('/all-blogs', async function(req, res, next) {
     
 })
 
+router.post('/blog-submit', async function(req, res, next) {
+    try {
+
+        const title = req.body.title;
+        const text = req.body.text;
+        const author = req.body.author;
+        const newDate = new Date()
+
+        const getPostCollectionLength = async () => {
+            try {
+                const collection = await blogsDB().collection('posts');
+                return await collection.count()
+            }catch(e){
+                console.log(500).send('error fetching data ' + e)
+            }
+        }
+        // console.log(await getPostCollectionLength())
+
+        const collection = await blogsDB().collection('posts');
+        const newPost = await collection.insertOne({
+            title:title,
+            text:text,
+            author:author,
+            createdAt: newDate.toLocaleString(),
+            lastModified: newDate.toLocaleString(),
+            id: await getPostCollectionLength() + 1
+        })
+        res.json({message:newPost})
+
+    }catch(e) {
+        console.log(e)
+        res.status(500).send('error fetching data ' + e)
+    }
+}) 
 module.exports = router;
 
-// var express = require("express");
-// var router = express.Router();
-// const { blogsDB } = require("../mongo");
-// router.get("/hello-blogs", (req, res) => {
-//   res.json({ message: "hello from express" });
-// });
-// router.get("/all-blogs", async (req, res) => {
-//   try {
-//     const limit = Number(req.query.limit);
-//     const skip = Number(req.query.limit) * (Number(req.query.page) - 1);
-//     const sortField = req.query.sortField;
-//     const sortOrder = req.query.sortOrder === "ASC" ? 1 : -1;
-//     const filterField = req.query.filterField;
-//     const filterValue = req.query.filterValue;
-//     const collection = await blogsDB().collection("posts");
-//     let filterObj = {};
-//     if (filterField && filterValue) {
-//       filterObj = { [filterField]: filterValue };
-//     }
-//     let sortObj = {};
-//     if (sortField && sortOrder) {
-//       sortObj = { [sortField]: sortOrder };
-//     }
-//     const posts = await collection
-//       .find(filterObj)
-//       .sort(sortObj)
-//       .limit(limit)
-//       .skip(skip)
-//       .toArray();
-//     res.json({ message: posts });
-//   } catch (e) {
-//     res.status(500).json("Error fetching posts." + e);
-//   }
-// });
-// module.exports = router;
